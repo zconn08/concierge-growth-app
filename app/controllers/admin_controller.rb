04016689ee20419101adb00e704840283759dc32
Zachary Conn <zconn08@gmail.com>
2015-11-09 21:50:29 -0800

    Add raw numbers table and style page

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,9 +2,9 @@ class AdminController < ApplicationController
 
   def index
     #Queries
-    events_total = Event.group(:event_type).count
+    @events_total = Event.group(:event_type).count
     events_by_rating = Event.joins(:rating).group([:event_type, :rating]).count
-    unique_events = Event.group(:event_type).distinct.count(:user_id)
+    @unique_events = Event.group(:event_type).distinct.count(:user_id)
     ratings_count = Rating.group(:rating).distinct.count(:rater)
     referral_counts = User.where("referrer_id > ?", 0).group(:referrer_id).count
     user_count = User.count
@@ -13,20 +13,20 @@ class AdminController < ApplicationController
     total_ratings = hash_sum_values(ratings_count)
 
     @submitted_rating_percent = to_percent(
-      unique_events["Submitted Rating"],
-      unique_events["Rating Page View"]
+      @unique_events["Submitted Rating"],
+      @unique_events["Rating Page View"]
     )
     @percent_four_or_five_ratings = to_percent(
       ratings_count[4].to_i + ratings_count[5].to_i,
       total_ratings
     )
     @page_views_per_invite_link_displayed = to_multiplier(
-      events_total["Invite Page View"],
+      @events_total["Invite Page View"],
       events_by_rating[["Submitted Rating",4]].to_i + events_by_rating[["Submitted Rating",5]].to_i
     )
     @signups_per_invite_page_view = to_percent(
-      events_total["User Signed Up"],
-      events_total["Invite Page View"]
+      @events_total["User Signed Up"],
+      @events_total["Invite Page View"]
     )
 
     #Conversion on Sign Up Page
@@ -73,14 +73,19 @@ class AdminController < ApplicationController
     )
 
     @page_views_by_rating = [
-      [4, @page_views_per_invite_link_displayed_four],
-      [5, @page_views_per_invite_link_displayed_five]
+      ["4 Rating", @page_views_per_invite_link_displayed_four],
+      ["5 Rating", @page_views_per_invite_link_displayed_five]
     ]
 
     #Top Users Driving Sign Up
 
-    @top_ten_percent_of_referrals = percent_of_referrals(referral_counts, user_count, 0.1)
-    @top_twenty_percent_of_referrals = percent_of_referrals(referral_counts, user_count, 0.2)
+    top_ten_percent_of_referrals = percent_of_referrals(referral_counts, user_count, 0.1)
+    top_twenty_percent_of_referrals = percent_of_referrals(referral_counts, user_count, 0.2)
+
+    @percent_of_users_driving_sign_up = [
+      ["Percent of Signups From Top 10%", top_ten_percent_of_referrals],
+      ["Percent of Signups From Top 20%", top_twenty_percent_of_referrals],
+    ]
 
     #Ratings Percent Breakdown
     @ratings_percent_breakdown = ratings_count.map do |rating, num_ratings|
